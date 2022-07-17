eed956e62b4fbb196a65e883f46556515b3d16bc
Crowing22192 <102896763+Crowing22192@users.noreply.github.com>
2022-07-17 17:16:02 +0700

    Delete slashCommands/ultillity directory

--- slashCommands/ultillity/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: 'Xem độ trễ của bot',
-    type: 'CHAT_INPUT',
-    run: async (client, interaction) => {
-        interaction.reply(`<:go_YaePing:954972099087335434> Pong! \`${client.ws.ping}ms\` `);
-    },
-};
